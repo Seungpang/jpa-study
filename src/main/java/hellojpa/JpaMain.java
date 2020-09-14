99b8332339de32482962baab0fec507d6a37d041
Seungpang <obey1342@gmail.com>
2020-09-14 21:38:32 +0900

    영속성 컨텍스트 테스트#2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,14 +17,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
             //영속
-            em.persist(member);
+            /* 영속 컨텍스트 동일성 보장
+            Member findMember1 = em.find(Member.class, 100L);
+            Member findMember2 = em.find(Member.class, 100L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
+            */
+
+            /* 쓰기지연
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("================");
+            */
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            if( member.getName().equals("ZZZZZ")){
+                em.persist(member);
+            }
+
+            System.out.println("================");
 
             tx.commit(); // 커밋시점에 DB에 저장된다.
         } catch (Exception e) {
