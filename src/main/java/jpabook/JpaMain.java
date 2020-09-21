910c0d817f5ce024e1587a330631fa2ab68f08af
Seungpang <obey1342@gmail.com>
2020-09-21 23:30:35 +0900

    양방향 연관관계 테스트2

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -2,6 +2,7 @@ package jpabook;
 
 
 import hellojpa.Member;
+import hellojpa.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,6 +20,26 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            //team.getMembers().add(member);
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            //member.changeTeam(team); 연관관계 메서드
+            em.persist(member);
+
+            team.addMember(member); //연관관계 메서드 (두곳중 한곳에서만)
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            for(Member m : members){
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
+
             tx.commit(); // 커밋시점에 DB에 저장된다.
         } catch (Exception e) {
             tx.rollback();
