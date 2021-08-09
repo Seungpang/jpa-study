6eed3916dd055e19f47e82df8703c1c06dda9018
Seungpang <obey1342@gmail.com>
2021-08-09 17:27:47 +0900

    조인

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,27 +19,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            for (int i=0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
+            String query = "select m from Member m left join Team t on m.username = t.name";
             List<Member> result = em
-                .createQuery("select m from Member m order by m.age desc", Member.class)
-                .setFirstResult(0)
-                .setMaxResults(10)
+                .createQuery(query, Member.class)
                 .getResultList();
 
             System.out.println("result = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
 
             tx.commit(); // 커밋시점에 DB에 저장된다.
         } catch (Exception e) {
